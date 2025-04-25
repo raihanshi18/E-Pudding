94a04bdfca8a51b15b1a61a5e0368b5d497093d2
unknown <cikofishh@gmail.com>
2025-04-25 19:32:33 +0700

    Style: Restyling login and register pages

--- resources/js/Pages/Register.jsx
+++ resources/js/Pages/Register.jsx
@@ -1,29 +1,81 @@
+import { ProfileCircle, Lock } from 'iconsax-reactjs'
 import ImgForm from '../../assets/Image-Form.svg'
 
 const Register = () => {
   return (
     <section className='flex justify-center items-center bg-Primary h-screen w-full'>
-    <div className='flex w-[60%] h-[65%] shadow-lg rounded-lg overflow-hidden'>
-      <img src={ImgForm} className='h-full'/>
-      <div className='flex flex-col justify-center items-center w-full h-full bg-Secondary'>
-        <h1 className='font-bold text-3xl mb-10'>Login Page</h1>
-        <div>
-          <div className='flex flex-col mb-3'>
-            <label htmlFor="username" className='font-semibold'>Username</label>
-            <input className='border-none rounded-lg w-64' type="text" name='username' id='username' placeholder='Type your username' required/>
-          </div>
-          <div className='flex flex-col mb-3'>
-            <label htmlFor="username" className='font-semibold'>Password</label>
-            <input className='border-none rounded-lg w-64' type="password" name='username' id='username' placeholder='Type your Password' required/>
-          </div>
-          <div className='flex flex-col gap-3'>
-            <button className='p-3 bg-Primary rounded-xl hover:bg-[#fff5bd]'>Sign In</button>
-            <button className='p-3 bg-Primary rounded-xl hover:bg-[#fff5bd]'>Sign Up</button>
+      <div className='flex w-[60%] h-[65%] shadow-lg rounded-lg overflow-hidden'>
+        <img src={ImgForm} className='h-full' />
+        <div className='flex flex-col justify-center items-center w-full h-full bg-Secondary'>
+          <h1 className='font-bold text-4xl mb-7 text-white'>Register</h1>
+          <div>
+            <div className='relative w-64 mb-2'>
+              <label htmlFor="username" className='font-semibold mb-1 block'>Username</label>
+              <div className='relative'>
+                <ProfileCircle
+                  size="20"
+                  color="#555555"
+                  variant="Outline"
+                  className='absolute top-1/2 left-3 transform -translate-y-1/2 pointer-events-none'
+                />
+                <input
+                  type="text"
+                  name='username'
+                  id='username'
+                  className='border-none rounded-md w-full pl-10 py-2 focus:outline-none focus:ring-2 focus:ring-Primary'
+                  placeholder='Username'
+                  required
+                />
+              </div>
+            </div>
+
+            <div className='relative w-64 mb-2'>
+              <label htmlFor="create-password" className='font-semibold mb-1 block'>Create Password</label>
+              <div className='relative'>
+                <Lock
+                  size="20"
+                  color="#555555"
+                  variant="Outline"
+                  className='absolute top-1/2 left-3 transform -translate-y-1/2 pointer-events-none'
+                />
+                <input
+                  type="password"
+                  name='create-password'
+                  id='create-password'
+                  className='border-none rounded-md w-full pl-10 py-2 focus:outline-none focus:ring-2 focus:ring-Primary'
+                  placeholder='Create Password'
+                  required
+                />
+              </div>
+            </div>
+
+            <div className='relative w-64 mb-4'>
+              <label htmlFor="confirm-password" className='font-semibold mb-1 block'>Confirm Password</label>
+              <div className='relative'>
+                <Lock
+                  size="20"
+                  color="#555555"
+                  variant="Outline"
+                  className='absolute top-1/2 left-3 transform -translate-y-1/2 pointer-events-none'
+                />
+                <input
+                  type="password"
+                  name='confirm-password'
+                  id='confirm-password'
+                  className='border-none rounded-md w-full pl-10 py-2 focus:outline-none focus:ring-2 focus:ring-Primary'
+                  placeholder='Confirm Password'
+                  required
+                />
+              </div>
+            </div>
+
+            <div className='flex flex-col gap-3 mt-5'>
+              <a href="/login" className='p-2 mx-10 bg-Primary rounded-xl hover:bg-[#fff5bd] flex justify-center'>Register</a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
   )
 }
 
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
-import Register from "./Pages/Auth/Register";
+import Register from "./Pages/Register";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
     <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
     </Routes>
