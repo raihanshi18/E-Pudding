10ed9a7a087e188eaf9251dad4ee68c92d8fdf2b
Rifandiysf <rifandiyusuf47@gmail.com>
2025-06-05 21:28:18 +0700

    add: route /me & /logout

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PuddingController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'store']);
+    Route::get('me', [AuthController::class, 'show']);
+    Route::get('logout', [AuthController::class, 'destroy']);
 });
 Route::middleware(['auth:sanctum', 'role:seller'])->prefix('pudding')->group(function () {
     Route::post('/', [PuddingController::class, 'store']);
