f551bf100b237f206715290e3a6d353851530235
Rifandiysf <rifandiyusuf47@gmail.com>
2025-06-05 21:30:12 +0700

    feat: consume api users

--- resources/js/Pages/Register.jsx
+++ resources/js/Pages/Register.jsx
@@ -1,10 +1,34 @@
 import { ProfileCircle, Lock } from 'iconsax-react'
 import ImgForm from '../../assets/Image-Form.svg'
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
+  const [isEmail, setIsEmail] = useState("")
+  const [isName, setIsName] = useState("")
+  const [isPassword, setIsPassword] = useState("")
+  const [isPasswordConfirm, setIsPasswordConfirm] = useState("")
+  const navigate = useNavigate()
+
+  const UserRegister = async(e) => {
+    e.preventDefault()
+    try {
+      await axios.post("http://127.0.0.1:8000/api/register", {
+        email: isEmail,
+        name: isName,
+        password: isPassword,
+        password_confirmation: isPasswordConfirm,
+      })
+      navigate("/login")
+    } catch (error) {
+      throw new error('register failed')
+    }
+  }
+
   return (
     <section className='flex justify-center items-center bg-Primary h-screen w-full'>
-      <div className='flex w-[60%] h-[65%] shadow-lg rounded-lg overflow-hidden'>
+      <form onSubmit={UserRegister} className='flex w-[60%] h-[65%] shadow-lg rounded-lg overflow-hidden'>
         <img src={ImgForm} className='h-full' />
         <div className='flex flex-col justify-center items-center w-full h-full bg-Secondary'>
           <h1 className='font-bold text-4xl mb-7 text-white'>Register</h1>
@@ -24,6 +48,30 @@ const Register = () => {
                   id='username'
                   className='border-none rounded-md w-full pl-10 py-2 focus:outline-none focus:ring-2 focus:ring-Primary'
                   placeholder='Username'
+                  value={isName}
+                  onChange={(e) => setIsName(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+
+            <div className='relative w-64 mb-2'>
+              <label htmlFor="email" className='font-semibold mb-1 block'>Email</label>
+              <div className='relative'>
+                <ProfileCircle
+                  size="20"
+                  color="#555555"
+                  variant="Outline"
+                  className='absolute top-1/2 left-3 transform -translate-y-1/2 pointer-events-none'
+                />
+                <input
+                  type="text"
+                  name='email'
+                  id='email'
+                  className='border-none rounded-md w-full pl-10 py-2 focus:outline-none focus:ring-2 focus:ring-Primary'
+                  placeholder='email'
+                  value={isEmail}
+                  onChange={(e) => setIsEmail(e.target.value)}
                   required
                 />
               </div>
@@ -44,6 +92,8 @@ const Register = () => {
                   id='create-password'
                   className='border-none rounded-md w-full pl-10 py-2 focus:outline-none focus:ring-2 focus:ring-Primary'
                   placeholder='Create Password'
+                  value={isPassword}
+                  onChange={(e) => setIsPassword(e.target.value)}
                   required
                 />
               </div>
@@ -64,17 +114,19 @@ const Register = () => {
                   id='confirm-password'
                   className='border-none rounded-md w-full pl-10 py-2 focus:outline-none focus:ring-2 focus:ring-Primary'
                   placeholder='Confirm Password'
+                  value={isPasswordConfirm}
+                  onChange={(e) => setIsPasswordConfirm(e.target.value)}
                   required
                 />
               </div>
             </div>
 
             <div className='flex flex-col gap-3 mt-5'>
-              <a href="/login" className='p-2 mx-10 bg-Primary rounded-xl hover:bg-[#fff5bd] flex justify-center'>Register</a>
+              <button type='submit' className='p-2 mx-10 bg-Primary rounded-xl hover:bg-[#fff5bd] flex justify-center'>Register</button>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </section>
   )
 }
