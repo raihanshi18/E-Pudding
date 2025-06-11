04637f30f4297a2a1893f8d507f89eaed47b05b1
Rifandiysf <rifandiyusuf47@gmail.com>
2025-06-11 17:51:51 +0700

    style: create menu ui

--- resources/js/Pages/Menu.jsx
+++ resources/js/Pages/Menu.jsx
@@ -6,7 +6,25 @@ const Menu = () => {
         <>
             <SideBar />
             <section className="flex flex-col min-h-[800px] ml-[300px] mr-[20px] my-6 max-sm:mx-[10px] max-sm:mb-[80px]">
-              
+                <div className="h-screen flex gap-20">
+                    <div className="w-72 h-[300px] bg-Secondary rounded-xl shadow-lg">
+                        <div className="w-full h-[190px] bg-Tertiary rounded-t-xl">
+                            a
+                            <img src="" alt="" />
+                        </div>
+                        <div className="px-3 py-1 flex flex-col justify-between h-[105px]">
+                            <div className="">
+                                <h1 className="text-lg font-semibold">Pudding</h1>
+                                <p>Enak dan bergizi baik untuk badan</p>
+                            </div>
+                            <div className="flex justify-end">
+                                <button className="py-1 items-end px-5 rounded-md bg-Tertiary">
+                                    See Detail
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     );
