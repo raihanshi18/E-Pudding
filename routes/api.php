437a33a4aed0ff82bd0daf45a435e14514e432fb
Rifandiysf <rifandiyusuf47@gmail.com>
2025-06-11 17:53:30 +0700

    add: route transactions

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,21 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PuddingController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('guest')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'store']);
     Route::get('me', [AuthController::class, 'show']);
     Route::get('logout', [AuthController::class, 'destroy']);
+});
+Route::middleware('guest')->prefix('transactions')->group(function () {
+    Route::get('/', [TransactionController::class, 'index']);
+    Route::get('/{id}', [TransactionController::class, 'show']);
+    Route::post('/', [TransactionController::class, 'store']);
+    Route::put('/{id}', [TransactionController::class, 'update']);
+    Route::delete('/{id}', [TransactionController::class, 'destroy']);
 });
 Route::middleware(['auth:sanctum', 'role:seller'])->prefix('pudding')->group(function () {
     Route::post('/', [PuddingController::class, 'store']);
